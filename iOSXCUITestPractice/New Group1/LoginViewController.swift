01e1af8c9774a95434723e4280dbb7f2d9f1fb87
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-25 14:34:20 +0900

    プロジェクト作成

---
+++ iOSXCUITestPractice/New Group1/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  iOSXCUITestPractice
+//
+//  Created by 坂本龍哉 on 2021/09/25.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
