592790c1c8372ae95ea751b968165545fe3e7844
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-25 14:59:28 +0900

    トップ画面を作成

---
+++ iOSXCUITestPractice/New Group1/TopViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TopViewController.swift
+//  iOSXCUITestPractice
+//
+//  Created by 坂本龍哉 on 2021/09/25.
+//
+
+import UIKit
+
+final class TopViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
