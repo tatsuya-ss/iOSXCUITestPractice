c62f4d3bc8f5f8f9b3327380f3500c86c1c42cd3
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-25 15:01:36 +0900

    トップ画面とログイン画面のアウトレット接続

--- iOSXCUITestPractice/New Group1/LoginViewController.swift
+++ iOSXCUITestPractice/New Group1/LoginViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 final class LoginViewController: UIViewController {
 
+    @IBOutlet private weak var nameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-    
+
+    @IBAction private func loginButtonDidTapped(_ sender: Any) {
+        
+    }
+
 }
